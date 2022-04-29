f47115997f35a08be060794f45449aac5af36026
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-04-29 20:52:51 +0200

    chore: simplify loading of api

--- src/routes/access/role/AccessRole.tsx
+++ src/routes/access/role/AccessRole.tsx
@@ -1,30 +1,25 @@
-import { getDataControllerByName, GridControllerButtons, loadDataController, SimpleHtmlGrid } from "@rad-tools";
-import React, { useState } from "react";
+import { ApiLoader, GridControllerButtons, SimpleHtmlGrid } from "@rad-tools";
+import React from "react";
 
 export function AccessRole() {
-    const [reload, setReload] = useState(true);
     const controllerName = "WEB_ROLE";
-    const controller = getDataControllerByName(controllerName);
-
-    if (!controller.gridInterface) {
-        setTimeout(() => {
-            loadDataController(controllerName).then(() => {
-                setReload(reload ? false : true);
-            });
-        });
-    } else {
-        const gridInterface = controller.gridInterface;
-        return (
-            <div className="flex flex-row h-full mt-2 w-full">
-                <div className="flex flex-col m-2">
-                    <GridControllerButtons dataSet={controllerName} />
-                </div>
-                <SimpleHtmlGrid
-                    className="simple-html-grid flex-grow m-3 mb-5"
-                    interface={gridInterface}
-                ></SimpleHtmlGrid>
-            </div>
-        );
-    }
-    return null;
+    return (
+        <ApiLoader
+            controllerName={controllerName}
+            callback={(controller) => {
+                const gridInterface = controller.gridInterface;
+                return (
+                    <div className="flex flex-row h-full mt-2 w-full">
+                        <div className="flex flex-col m-2">
+                            <GridControllerButtons dataSet={controllerName} />
+                        </div>
+                        <SimpleHtmlGrid
+                            className="simple-html-grid flex-grow m-3 mb-5"
+                            interface={gridInterface}
+                        ></SimpleHtmlGrid>
+                    </div>
+                );
+            }}
+        ></ApiLoader>
+    );
 }
--- src/routes/access/user/accessUser.tsx
+++ src/routes/access/user/accessUser.tsx
@@ -1,31 +1,26 @@
-import { getDataControllerByName, GridControllerButtons, loadDataController, SimpleHtmlGrid } from "@rad-tools";
-import React, { useState } from "react";
+import { GridControllerButtons, SimpleHtmlGrid, ApiLoader } from "@rad-tools";
+import React from "react";
 
 export function AccessUser() {
-    const [reload, setReload] = useState(true);
-
     const controllerName = "WEB_USER";
-    const controller = getDataControllerByName(controllerName);
 
-    if (!controller.gridInterface) {
-        setTimeout(() => {
-            loadDataController(controllerName).then(() => {
-                setReload(reload ? false : true);
-            });
-        });
-    } else {
-        const gridInterface = controller.gridInterface;
-        return (
-            <div className="flex flex-row h-full mt-2 w-full">
-                <div className="flex flex-col m-2">
-                    <GridControllerButtons dataSet={controllerName} />
-                </div>
-                <SimpleHtmlGrid
-                    className="simple-html-grid flex-grow m-3 mb-5"
-                    interface={gridInterface}
-                ></SimpleHtmlGrid>
-            </div>
-        );
-    }
-    return null;
+    return (
+        <ApiLoader
+            controllerName={controllerName}
+            callback={(controller) => {
+                const gridInterface = controller.gridInterface;
+                return (
+                    <div className="flex flex-row h-full mt-2 w-full">
+                        <div className="flex flex-col m-2">
+                            <GridControllerButtons dataSet={controllerName} />
+                        </div>
+                        <SimpleHtmlGrid
+                            className="simple-html-grid flex-grow m-3 mb-5"
+                            interface={gridInterface}
+                        ></SimpleHtmlGrid>
+                    </div>
+                );
+            }}
+        ></ApiLoader>
+    );
 }
