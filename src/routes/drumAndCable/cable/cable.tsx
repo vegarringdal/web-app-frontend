47be124069ecf60dd474bd4edc1b92cfb22facbc
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-05-07 11:17:36 +0200

    chore: add title test api

--- src/routes/drumAndCable/cable/cable.tsx
+++ src/routes/drumAndCable/cable/cable.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ApiLoader, GridControllerButtons, SimpleHtmlGrid } from "@rad-tools";
+import { ApiLoader, getApiConfig, GridControllerButtons, SimpleHtmlGrid } from "@rad-tools";
 
 export function Cable() {
     const controllerName = "CABLE";
@@ -9,14 +9,18 @@ export function Cable() {
             callback={(controller) => {
                 const gridInterface = controller.gridInterface;
                 return (
-                    <div className="flex flex-row h-full mt-2 w-full">
-                        <div className="flex flex-col m-2">
-                            <GridControllerButtons dataSet={controllerName} />
+                    <div className="flex flex-col w-full">
+                        <span className="m-auto text-xl">{getApiConfig(controllerName).api.title}</span>
+
+                        <div className="flex flex-row h-full mt-2 w-full">
+                            <div className="flex flex-col m-2">
+                                <GridControllerButtons dataSet={controllerName} />
+                            </div>
+                            <SimpleHtmlGrid
+                                className="simple-html-grid flex-grow m-3 mb-5"
+                                interface={gridInterface}
+                            ></SimpleHtmlGrid>
                         </div>
-                        <SimpleHtmlGrid
-                            className="simple-html-grid flex-grow m-3 mb-5"
-                            interface={gridInterface}
-                        ></SimpleHtmlGrid>
                     </div>
                 );
             }}
